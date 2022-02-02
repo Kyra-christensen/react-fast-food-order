514e17839c74b4f86afbe2d895ffd0d997c8dcd8
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-01 23:14:48 -0800

    imported useState, created images state and the react component that holds those images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import OrderImages from './OrderImages';
 
 function App() {
+  const [foodId, setFoodId] = useState(1);
+  const [sideId, setSideId] = useState(1);
+  const [drinkId, setDrinkId] = useState(1);
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <OrderImages 
+        foodId={foodId}
+        sideId={sideId}
+        drinkId={drinkId} />
     </div>
   );
 }
