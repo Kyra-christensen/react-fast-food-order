909c6a7442b8c33339e7c66dfd979642565dcec1
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-01 23:41:32 -0800

    added an orderList component that takes in an instructions array, added instructions array state

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import './App.css';
 import { useState } from 'react';
 import OrderImages from './OrderImages';
 import OrderName from './OrderName';
+import OrderList from './OrderList';
 
 function App() {
   const [foodId, setFoodId] = useState(1);
   const [sideId, setSideId] = useState(1);
   const [drinkId, setDrinkId] = useState(1);
   const [orderName, setOrderName] = useState('');
+  const [instructionsArray, setInstructionsArray] = useState([]);
     
   return (
     <div className="App">
+
       <h3>Order for valued customer: {orderName}</h3>
         
       <OrderImages 
         foodId={foodId}
         sideId={sideId}
         drinkId={drinkId} />
+
+      <OrderList instructionsArray={instructionsArray} />
     </div>
   );
 }
