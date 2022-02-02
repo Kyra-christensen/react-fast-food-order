087e670e9ec5e5d62fe07e6b07611321d63387a0
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-01 23:33:48 -0800

    added orderName component and state, imported OrderName file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import { useState } from 'react';
 import OrderImages from './OrderImages';
+import OrderName from './OrderName';
 
 function App() {
   const [foodId, setFoodId] = useState(1);
   const [sideId, setSideId] = useState(1);
   const [drinkId, setDrinkId] = useState(1);
+  const [orderName, setOrderName] = useState('');
     
   return (
     <div className="App">
+      <h3>Order for valued customer: {orderName}</h3>
+        
       <OrderImages 
         foodId={foodId}
         sideId={sideId}
